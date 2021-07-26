4ec03763feb9aa227143236213053d019f53d972
nikethanrai <nikethanrai58@gmail.com>
2021-07-26 18:09:34 +0530

    stuck at input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [billAmt,setbillAmt]= useState('')
+
+  function inputHandler(input){
+    setbillAmt(input)
+    
+  }
   return (
     <div className="App">
-      hello
+      <h1>Monay Monay!!</h1>
+      <p>Enter the bill amount and the cash given by the customer and know the minimum number of notes to return</p>
+    <div className='label'> Bill Amount:
+    <input onInput={()=>inputHandler()} type='number' id='billAmt'/>
+    <button id='next'>Next</button>
+    <h3>{billAmt}</h3>
+
+    </div>
     </div>
   );
 }
